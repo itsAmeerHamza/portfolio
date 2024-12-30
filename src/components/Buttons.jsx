1a8ae280da720c2f55eee8235cadf06fd42b9363
itsAmeerHamza <hamzaameer24@gmail.com>
2024-12-30 13:57:01 +0530

    On branch master

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,11 +4,39 @@ import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
 
 const Buttons = () => {
   return (
-    <div className="icon-container">
-      <FontAwesomeIcon id="github" icon={faSquareGithub} />
-      <FontAwesomeIcon id="linkedin" icon={faLinkedin} />
-      <FontAwesomeIcon id="email" icon={faEnvelope} />
-      <FontAwesomeIcon id="resume" icon={faFile} style={{ color: "#ffffff" }} />
+    <div id="icon-container">
+      <a
+        id="github"
+        href="https://github.com/itsAmeerHamza"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faSquareGithub} />
+      </a>
+      <a
+        id="linkedin"
+        href="https://www.linkedin.com/in/itsameerhamza"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a
+        id="email"
+        href="mailto:hamzaameer24@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+      <a
+        id="resume"
+        href="https://drive.google.com/file/d/1MzW3M3b81GqLKkSyWnii759Swq6WLtmt/view?usp=drive_link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faFile} style={{ color: "#ffffff" }} />
+      </a>
     </div>
   );
 };
